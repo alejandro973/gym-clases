207b0c9eb5f9edd41e5f994cbd742c931c1884b4
alejandro973 <os.carrizo@duocuc.cl>
2026-06-30 18:44:16 -0400

    Modificacion de feing

--- src/main/java/com/gym/clases/client/EntrenadorClient.java
+++ src/main/java/com/gym/clases/client/EntrenadorClient.java
@@ -4,10 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "entrenadores", url = "http://localhost:8082")
+@FeignClient(name = "entrenadores", url = "${FEIGN_URL_ENTRENADORES:http://localhost:8082}")
 public interface EntrenadorClient {
 
 @GetMapping("/api/entrenadores/buscar/{run}")
     EntrenadorDto obtenerEntrenadorPorRun(@PathVariable("run") String run);
 
 }
+
+//
